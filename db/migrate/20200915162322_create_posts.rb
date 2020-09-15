520fb61ca4e5117d5ff831b142801fc6412b56b2
carriemathieu <carrieburks@gmail.com>
2020-09-15 13:10:45 -0400

    migrations, index with links

--- db/migrate/20200915162322_create_posts.rb
+++ db/migrate/20200915162322_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :content
+      t.string :feeling
+      t.string :topic
+      t.integer :user_id
       t.timestamps null: false
     end
   end
